249e92f3502f1a59c399be0b0e1344bc2bbc0f03
yazidpy <yazidaliche8@gmail.com>
2024-07-20 15:37:11 +0200

    updated

--- Api/urls.py
+++ Api/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('store/',include('Store.urls')),
     path('auth/',include('djoser.urls')),
     path('auth/',include('djoser.urls.jwt'))
