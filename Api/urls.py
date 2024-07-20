d4d8208fbe3d2cf4cbb47fbf230f39451a700083
yazidpy <yazidaliche8@gmail.com>
2024-07-20 15:27:54 +0200

    updated

--- Api/urls.py
+++ Api/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('store/',include('Store.urls')),
     path('auth/',include('djoser.urls')),
     path('auth/',include('djoser.urls.jwt'))
@@ -12,4 +12,4 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
